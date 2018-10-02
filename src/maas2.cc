feea272c08eabaca2581ae8af37477d33271e66c
Brent Maas <brent.maas@hotmail.com>
2018-10-02 12:23:23 +0200

    Lychrel onzin en fix gitignore

--- src/maas2.cc
+++ src/maas2.cc
@@ -24,7 +24,45 @@ void infoBlokje(){
 	"Laatst bewerkt: 20/09/2018\n"
 	"////////////////////////////////////////////////\n" << std::endl;
 }
-		
+
+//Machtfunctie voor integers voor positieve exp
+int pow(int base, int exp){
+	int res = 1;
+	for(int i = 0;i < exp;i++) res *= base;
+	return res;
+}
+
+//Returnt het getal van i-ste orde van getal
+//Bijv: intAt(12345, 3) = 2, intAt(12345, 1) = 4
+int intAt(int getal, int i){
+	return getal % pow(10, i + 1) / pow(10, i);
+}
+
+//Draai een integer om als ware het een string
+int reverse(int getal){
+	int order = 10;
+	while(getal % pow(10, order) == getal) order--;
+	int res = 0;
+	for(int i = 0;i < order + 1;i++) res += intAt(getal, order - i) * pow(10, i);
+	return res;
+}
+
+//Returnt het aantal iteraties voordat het gegeven getal een palindroom is.
+//In het geval dat dit te lang duurt (boven INT_MAX uitkomt), wordt de negatieve
+//waarde van de huidige iteratie - 1 gegeven.
+void lychrel(int getal){
+	int buffer = getal, rev, i;
+	for(i = 0;rev >= 0 && buffer >= 0 && getal <= INT_MAX - rev;i++){
+		rev = reverse(buffer);
+		if(buffer == rev){
+			std::cout << getal << " is een Lychrel-getal na " << i << " iteraties" << std::endl;
+			return;
+		}
+		buffer += rev;
+	}
+	std::cout << getal << " is een potentieel Lychrel-getal na " << i << " iteraties (INT_MAX bereikt)" << std::endl;
+}
+
 void coderen(){
 	
 }
