1f517e18d51333ec04f339c2f66b7323872ac1ef
Brent Maas <brent.maas@hotmail.com>
2018-10-04 12:24:31 +0200

    Coderen werkt nu, simpel2018.txt heeft verkeerd aantal regels

--- src/maas2.cc
+++ src/maas2.cc
@@ -4,7 +4,7 @@
  * Functie: 
  * Getest op: Windows 10 + MSys2 (MinGW64) met g++ 7.3.0
  * Getest met: g++ -o Assignment1 maas1.cc -std=c++17 -Wall -Wextra
- * Laatst bewerkt: 20/09/2018
+ * Laatst bewerkt: 04/10/2018
  */
 
 #include <iostream>
@@ -63,12 +63,74 @@ void lychrel(int getal){
 	std::cout << getal << " is een potentieel Lychrel-getal na " << i << " iteraties (INT_MAX bereikt)" << std::endl;
 }
 
-void coderen(){
-	
+//Geeft de gecodeerde versie van karakter, als karakter het index-de karakter in het
+//bestand is met de gegeven pincode
+char codeerEnkel(char karakter, int pincode, int index){
+	return (karakter + intAt(pincode, 3 - (index % 4))) % 256;
 }
 
-void decoderen(){
+int coderen(const char* invoerBestand, const char* uitvoerBestand, const int pincode){
+	std::fstream invoer(invoerBestand, std::ios_base::in);	
+	if(!invoer.is_open()){
+		std::cout << "Kon invoerbestand niet openen: " << invoerBestand << std::endl;
+		return 1;
+	}
+	std::fstream uitvoer(uitvoerBestand, std::ios_base::out);
+	if(!uitvoer.is_open()){
+		std::cout << "Kon uitvoerbestand niet openen: " << uitvoerBestand << std::endl;
+		return 1;
+	}
 	
+	char current;
+	int code = pincode, regels = 0, karakters = 0, index = 0, numBuf = 0;
+	while(true){
+		current = invoer.get();
+		if(invoer.eof()) break;
+		if(current == '\n'){
+			//std::cout << current;
+			uitvoer << current;
+			index = 0;
+			regels++;
+		}else{
+			//std::cout << codeerEnkel(current, code, index);
+			uitvoer << codeerEnkel(current, code, index);
+			index++;
+		}
+		//0 is op waarde 48 in de asciitabel
+		if(current >= '0' && current <= '9') numBuf = numBuf * 10 + (current - 48);
+		else if(numBuf != 0){
+			lychrel(numBuf);
+			if(numBuf > 0 && numBuf < 10000){
+				code = numBuf;
+				index = 0;
+			}
+			numBuf = 0;
+		}
+		
+		karakters++;
+	}
+	
+	std::cout << "Het invoerbestand had " << karakters << " karakters in " << regels << " regels." << std::endl;
+	
+	std::cout << std::endl;
+	
+	invoer.close();
+	uitvoer.close();
+	
+	return 0;
+}
+
+//Geeft de gedecodeerde versie van karakter, als karakter het index-de karakter in het
+//bestand is met de gegeven pincode
+char decodeerEnkel(char karakter, int pincode, int index){
+	//De +256 omdat -1 % 256 = -1 en dat is geen geldige asciikarakter
+	return (karakter - intAt(pincode, 3 - (index % 4)) + 256) % 256;
+}
+
+int decoderen(const char* invoerBestand, const char* uitvoerBestand, const int pincode){
+	
+	
+	return 0;
 }
 
 int main(){
@@ -77,13 +139,24 @@ int main(){
 	std::cout << "Wilt u een bestand [C]oderen of [D]ecoderen? ";
 	char keuze;
 	std::cin >> keuze;
-	
-	if(keuze == 'c' || keuze == 'C') coderen();
-	else if(keuze == 'd' || keuze == 'D') decoderen();
-	else{
+	if(keuze != 'c' && keuze != 'C' && keuze != 'd' && keuze != 'D'){
 		std::cout << "Ongeldige keuze: " << keuze << std::endl;
 		return 1;
 	}
 	
-	return 0;
+	std::string invoerFile, uitvoerFile;
+	int pincode;
+	std::cout << "Invoerbestand: ";
+	std::cin >> invoerFile;
+	std::cout << "Uitvoerbestand: ";
+	std::cin >> uitvoerFile;
+	std::cout << "Pincode: ";
+	std::cin >> pincode;
+	if(pincode < -1 && pincode > 9999){
+		std::cout << "Ongeldige pincode: " << pincode << std::endl;
+		return 1;
+	}
+	
+	if(keuze == 'c' || keuze == 'C') return coderen(invoerFile.c_str(), uitvoerFile.c_str(), pincode);
+	return decoderen(invoerFile.c_str(), uitvoerFile.c_str(), pincode);
 }
